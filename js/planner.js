b3b5c6930e48ec3bf01cb9cb19e60dc3e45b8da8
Romain B <>
2025-02-14 17:12:22 +0100

    Made a working prototype for adding/removing events and partial conflict checking

--- js/planner.js
+++ js/planner.js
@@ -1,107 +1,67 @@
+// Setup variables and functions
+const wk = [0,1,2,3,4,5,6].map(num => "2025-07-2"+num); // dates of the week
+
+/* Function to make a recurring event object from basic info:
+	id: event ID for handling ("g_"+id to catch all recurring events)
+	title: display text on the calendar
+	sTime, eTime: start/end times
+	sDate, eDate: start/end dates (defaults to start/end of week)
+	dow: days of the week on which the event occurs, within above dates (sun=0, mon=1, ...)
+	col: display color of the event in the calendar
+	price: price for the
+*/			
+function mk_event_r(id, title, sTime, eTime, 
+					price,
+					col="royalblue",
+					sDate=wk[0], eDate=wk[6], 
+					dow=[0,1,2,3,4,5]){
+	return {
+			id: id, title: title, 	  
+			groupId: "g_".concat(id), 
+			startTime: sTime, endTime: eTime,
+			startRecur: sDate, endRecur: eDate,
+			daysOfWeek: dow,
+			color: col,
+			extendedProps:{
+				price: (price/dow.length)
+			},
+			editable: false, // we don't want users moving stuff around
+	};             
+}
+/* Function to make a simple event object from basic info:
+	id: event ID for handling 
+	title: display text on the calendar
+	sTime, eTime: start/end times
+	date: date of the event
+	col: display color of the event in the calendar		
+*/	
+function mk_event(id, title, sTime, eTime, date, price,
+				  col="royalblue"){
+	return {
+			id: id, title: title,
+			start: date.concat('T', sTime), 
+			end: date.concat('T', eTime),
+			color: col,
+			extendedProps:{
+				price: price
+			},
+			editable: false,
+	};               
+}
+	
+// Make the list of all event/activities
+const activity_list = {
+	"diapason": mk_event_r("diapason", "Choeur Diapa'Son", "14:00", "16:00", price=50, col='royalblue'),
+	"generason": mk_event_r("generason", "Choeur Généra'Son", "11:30", "13:00", price=45, col='firebrick'),
+	"enchantillages": mk_event_r("enchantillages", "Choeur Enchantillages", "14:00", "16:00", price=40, col="darkturquoise"),
+	"prog_solo": mk_event("prog_solo", "Progres'Son Individuel (Créneau 45min)", "10:00", "11:30", date=wk[1], price=20, col='turquoise'),
+	"prog_coll": mk_event("prog_coll", "Progres'Son Collectif", "11:40", "12:30", date=wk[1], price=10, col='green')
+};
+
+
+
+// Reactive elements
 document.addEventListener("DOMContentLoaded", function () {
-	
-	// Setup variables and functions
-	const wk = [0,1,2,3,4,5,6].map(num => "2025-07-2"+num); // dates of the week
-	
-	/* Function to make a recurring event object from basic info:
-		id: event ID for handling ("g_"+id to catch all recurring events)
-		title: display text on the calendar
-		sTime, eTime: start/end times
-		sDate, eDate: start/end dates (defaults to start/end of week)
-		dow: days of the week on which the event occurs, within above dates (sun=0, mon=1, ...)
-		col: display color of the event in the calendar
-	*/			
-	function mk_event_r(id, title, sTime, eTime, 
-						sDate=wk[0], eDate=wk[6], 
-						dow=[0,1,2,3,4,5], col="royalblue"){
-      return {
-				id: id, title: title, 	  // id is for handling, title is for display
-				groupId: "g_".concat(id), // to handle all the sub-events
-				startTime: sTime, endTime: eTime,
-				startRecur: sDate, endRecur: eDate,
-				daysOfWeek: dow,
-				color: col,
-				editable: false,
-      }                
-	}
-	/* Function to make a simple event object from basic info:
-		id: event ID for handling 
-		title: display text on the calendar
-		sTime, eTime: start/end times
-		date: date of the event
-		col: display color of the event in the calendar		
-	*/	
-  function mk_event(id, title, sTime, eTime, date, 
-					col="royalblue"){
-      return {
-      			id: id, title: title,
-				start: date.concat('T', sTime), 
-				end: date.concat('T', eTime),
-				color: col,
-				editable: false,
-      }                
-	}
-	
-	const activity_list = {
-    	"diapason":{
-			id: "diapason",
-			groupId: "g_diapason",
-			title: "Choeur Diapa'Son",
-					daysOfWeek: [0,1,2,3,4,5],
-			startRecur: wk[0],
-			endRecur: wk[6],
-			startTime: "14:00",
-			endTime: "16:00",
-			editable: false,
-			color: 'royalblue'
-		},
-		"generason":{
-			id: "generason",
-			groupId: "g_generason",
-			title: "Choeur Généra'Son",
-					daysOfWeek: [0,1,2,3,4,5],
-			startRecur: wk[0],
-			endRecur: wk[6],
-			startTime: "11:30",
-			endTime: "13:00",
-			editable: false,
-			color: 'deeppink'
-		},
-		"enchantillages":{
-			id: "enchantillages",
-			groupId: "g_enchantillages",
-			title: "Choeur Enchantillages",
-					daysOfWeek: [0,1,2,3,4,5],
-			startRecur: wk[0],
-			endRecur: wk[6],
-			startTime: "14:00",
-			endTime: "16:00",
-			editable: false,
-			color: 'deeppink'
-		},
-		"technique_solo":{
-			id: "technique_solo",
-			title: "Progres'Son Individuel (Créneau 45min)",
-			details:"",
-			start: wk[2].concat('T', "14:00"),
-			end: wk[2].concat('T', "16:00"),
-			editable: false, 
-			color: "chartreuse4"
-		},
-		"technique_groupe":{
-			id: "technique_solo",
-			title: "Progres'Son Collectif (45min)",
-			details:"",
-			start: wk[2].concat('T', "14:00"),
-			end: wk[2].concat('T', "16:00"),
-			editable: false, 
-			color: "chartreuse3"
-		}
-      
-    };
-	
-	
-	
 	
 	// get the calendar area of the webpage
     let calendarEl = document.getElementById("edt-display"); 
@@ -118,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
         events: []					 // will be defined dynamically.
     });
 
-    calendar.render();	// renders the calendar
+    calendar.render();	// show the calendar
 
     let selectedActivities = [];
 	
@@ -152,6 +112,17 @@ document.addEventListener("DOMContentLoaded", function () {
         mealsSelected = this.checked;
         updateUI();
     });
+	
+	function hasOverlap(newEvent) {
+    // Get all existing events from the calendar
+    existingEvents = calendar.getEvents();
+    
+    // Check each existing event for overlap
+    return existingEvents.some(existingEvent => {
+        return newEvent.start < existingEvent.end && 
+               newEvent.end > existingEvent.start;
+    });
+}
 
     function addActivity(activityName) {
         const activity = activities[activityName];
@@ -162,17 +133,11 @@ document.addEventListener("DOMContentLoaded", function () {
             let duration = activity.duration;
             let endTime = addMinutes(startTime, duration);
 
-            if (calendar.getEvents().some(event => event.startStr.includes(day) && event.startStr.includes(startTime))) {
+            if (hasOverlap(activity_list[activityName])) {
                 alert("❌ Activity conflict detected!");
                 return false;
             }
 
-            calendar.addEvent({
-                title: activityName,
-                start: `${day}T${startTime}`,
-                end: `${day}T${endTime}`,
-                color: "blue"
-            });
         }
 
         selectedActivities.push(activityName);
